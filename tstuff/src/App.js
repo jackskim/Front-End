8d705379551706f5194d85d858aa83a28a988fb5
seongck <seongk@pm.me>
2019-08-21 21:08:30 -0700

    Import Login component into App.js

--- tstuff/src/App.js
+++ tstuff/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './components/Login.js';
 
 function App() {
   return (
     <div className="App">
+      <Route 
+        exact
+        path="/login"
+        render={props => <Login {...props} /> }
+      />
     </div>
   );
 }
