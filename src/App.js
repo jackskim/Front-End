27fac159cd1a48f2e2c057b3ea0e07a5d112c3b6
seongck <seongk@pm.me>
2019-08-23 19:23:26 -0700

    Add Home component and route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './components/Login.js';
 import SignUp from './components/SignUp.js';
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div className="App">
+      <Route 
+        exact
+        path="/"
+        component={Home}
+      />
       <Route 
         exact
         path="/login"
