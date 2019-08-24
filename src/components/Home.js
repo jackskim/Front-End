5b0765904ef1722915c6193d17271b367a243fe2
seongck <seongk@pm.me>
2019-08-23 19:36:03 -0700

    Import <Navigation /> to <Home />

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Navigation from './Navigation.js';
+
+function Home() {
+  return (
+    <Container>
+      <Navigation />
+    </Container>
+  );
+}
+
+export default Home;
