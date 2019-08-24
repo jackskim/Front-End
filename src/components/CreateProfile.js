ddec1d305d02e39399d6f87971754314f7f5f7e3
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-08-24 11:49:06 -0500

    CreateProfile form is done, the axios call might need another look at it since i kind of guessed how it would work, its also missing the nav but we canadd that after we finish it,

---
+++ src/components/CreateProfile.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Formik, Form, Field, } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
+import { Container, Header, Button } from 'semantic-ui-react';
+
+const CreateProfileSchema =  Yup.object().shape({
+    phone: Yup.number().required("Please enter your phone number"),
+    street: Yup.string(),
+    city: Yup.string().string().required("Please enter your city"),
+    avatarUrl: Yup.string()
+});
+
+
+function CreateProfile (props) {
+
+    return (
+        < Container className='container' >
+            <Header as="h1">Create Profile</Header>
+                <Formik
+                    initialValues = {{ phone: '', street: '', city: '', avatarUrl: '' }}
+                    validationSchema = {CreateProfileSchema}
+                    onSubmit = {( values, { resetForm, setStatus }) => {
+                        axios
+                            .post("https://umts-backend.herokuapp.com/api/auth/profile", values)
+                            .then(res => {
+                                setStatus(res.data);
+                                resetForm();
+                                props.history.push('/Home');
+
+                            })
+                            .catch( err => {
+                                console.log(err);
+                                setStatus(err);
+                            })
+                    }}
+                >    
+                {({ isSubmitting, errors, touched }) => (
+                    <Form className='form'>
+                        <Field
+                            className='field'
+                            type='number'
+                            name='phone'
+                            placeholder='Phone Number'
+                        />
+                         {touched.phone && errors.phone && (<p className='form__error'>{errors.phone}</p>)}
+                        <Field
+                            className='field'
+                            type='text'
+                            name='street'
+                            placeholder='Street Name'
+                        />
+                         {touched.street && errors.street && (<p className='form__error'>{errors.street}</p>)}
+                        <Field
+                            className='field'
+                            type='text'
+                            name='city'
+                            placeholder='City Name'
+                        />
+                        {touched.city && errors.city && (<p className='form__error'>{errors.city}</p>)}
+                        <Field 
+                            className='field'
+                            type='text'
+                            name='avatarUrl'
+                            placeholder='Avatar'
+                        />
+                         {touched.avatarUrl && errors.avatarUrl && (<p className='form__error'>{errors.avatarUrl}</p>)}
+                        <Button className="button" type="submit" disabled={isSubmitting}>Submit</Button>
+                    </Form>
+                )}        
+            </Formik>         
+        </Container>
+    );
+                }
+
+                export default CreateProfile;
