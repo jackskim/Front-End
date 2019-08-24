876f52cbe6801fe00989672a01b1420787b38992
seongck <seongk@pm.me>
2019-08-23 21:14:27 -0700

    Add nav elements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,41 @@ import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 function Navigation() {
+  const token = localStorage.getItem('token');
+  console.log(token);
+
   return (
-    <div>
       <Menu>
+        <Menu.Item key='logo'>
+          <NavLink to="/">TSTUFF</NavLink>
+        </Menu.Item>
         <Menu.Item key='home'>
           <NavLink to="/">Home</NavLink>
         </Menu.Item>
+
+        { !token &&
+
+          <Menu.Item key='login'>
+            <NavLink to="/login">Login</NavLink>
+          </Menu.Item> &&
+
+          <Menu.Item key='signup'>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </Menu.Item>
+        }
+
+        { token &&
+          <Menu.Item key='list-your-item'>
+            <NavLink to="/">List Your Item</NavLink>
+          </Menu.Item> &&
+          <Menu.Item key='dashboard'>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </Menu.Item> &&
+          <Menu.Item key='logout'>
+            <NavLink to="/">Log Out</NavLink>
+          </Menu.Item> 
+        }
       </Menu>
-    </div>
   );
 }
 
