d008b4d296b1bf7ce33e1b6a7e037b2da996b586
seongck <seongk@pm.me>
2019-08-25 17:06:53 -0700

    Add Navigation back in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Navigation from './Navigation.js';
 
 function Home(props) {
   return (
     <Container>
+      <Navigation />
     </Container>
   );
 }
