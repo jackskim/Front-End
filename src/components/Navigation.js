a04e78c2e2b4ebaefb314627b33f191388d2e71a
seongck <seongk@pm.me>
2019-08-25 17:07:24 -0700

    Clean up Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,42 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
-function Navigation() {
-  const token = localStorage.getItem('token');
-  console.log(token);
-
+function Navigation(props) {
   return (
-      <Menu>
-        <Menu.Item key='logo'>
-          <NavLink to="/">TSTUFF</NavLink>
-        </Menu.Item>
-        <Menu.Item key='home'>
-          <NavLink to="/">Home</NavLink>
-        </Menu.Item>
-
-        { !token &&
-
-          <Menu.Item key='login'>
-            <NavLink to="/login">Login</NavLink>
-          </Menu.Item> &&
-
-          <Menu.Item key='signup'>
-            <NavLink to="/signup">Sign Up</NavLink>
-          </Menu.Item>
-        }
-
-        { token &&
-          <Menu.Item key='list-your-item'>
-            <NavLink to="/">List Your Item</NavLink>
-          </Menu.Item> &&
-          <Menu.Item key='dashboard'>
-            <NavLink to="/dashboard">Dashboard</NavLink>
-          </Menu.Item> &&
-          <Menu.Item key='logout'>
-            <NavLink to="/">Log Out</NavLink>
-          </Menu.Item> 
-        }
-      </Menu>
+    <Menu>
+      <Menu.Item key='logo'>
+        <NavLink to="/">TSTUFF</NavLink>
+      </Menu.Item>
+      <Menu.Item key='home'>
+        <NavLink to="/">Home</NavLink>
+      </Menu.Item>
+      <Menu.Item key='list--item'>
+        <NavLink to="/">List Item</NavLink>
+      </Menu.Item> 
+      <Menu.Item key='dashboard'>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      </Menu.Item>
+      <Menu.Item key='logout'>
+        <NavLink to="/">Log Out</NavLink>
+      </Menu.Item> 
+    </Menu>
   );
 }
 
