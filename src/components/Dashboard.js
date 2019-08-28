81f5a0f86644aa9b26d02ddd2da6f7da1794aed0
seongck <seongk@pm.me>
2019-08-27 18:48:41 -0700

    Add tab menu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Container, Header, Image } from 'semantic-ui-react';
+import { Container, Header, Image, Menu, Tab } from 'semantic-ui-react';
 import Navigation from './Navigation.js';
 import defaultAvatar from '../assets/default-avatar.png';
 
 function Dashboard(props) {
   console.log(props);
   const avatarSrc = props.user.avatarUrl ? props.user.avatarUrl : defaultAvatar;
+
+  const panes = [
+    { menuItem: ( 
+      <Menu.Item key='my-item-list'>
+        My Item List
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane></Tab.Pane>
+    },
+    { menuItem: ( 
+      <Menu.Item key='my-item-list'>
+        Update Profile 
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane></Tab.Pane>
+    }
+  ];
 
   return (
     <Container fluid>
@@ -15,6 +32,7 @@ function Dashboard(props) {
           <Header as="h2">
             Welcome back {props.user.firstName}!
           </Header>
+          <Tab panes={panes} /> 
         </Container>
     </Container>
     
