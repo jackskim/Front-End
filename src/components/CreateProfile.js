92a9adfe0b2c4b48253782379d7a39531f1e3931
seongck <seongk@pm.me>
2019-08-27 20:54:30 -0700

    Update form styles

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik, Form, Field, } from 'formik';
 import * as Yup from 'yup';
-import { Container, Header, Button } from 'semantic-ui-react';
+import { Grid, Segment, Header, Button } from 'semantic-ui-react';
 import axiosWithAuth from '../axiosWithAuth.js'
 
 const CreateProfileSchema =  Yup.object().shape({
@@ -15,8 +15,9 @@ const CreateProfileSchema =  Yup.object().shape({
 function CreateProfile (props) {
 
     return (
-        < Container className='container' >
-            <Header as="h1">Create Profile</Header>
+        <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle" >
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as="h2" style={{ color: '#2B4162' }}>Create Profile</Header>
                 <Formik
                     initialValues = {{ 
                         firstName: `${props.user.firstName}`,
@@ -46,75 +47,90 @@ function CreateProfile (props) {
                 >    
                 {({ isSubmitting, errors, touched }) => (
                     <Form className='form'>
-                        <Field
-                            className='field'
-                            type='text'
-                            name='firstName'
-                            placeholder='First Name'
-                        />
-                         {touched.firstName && errors.firstName && (<p className='form__error'>{errors.firstName}</p>)}
-                        <Field
-                            className='field'
-                            type='text'
-                            name='lastName'
-                            placeholder='Last Name'
-                        />
-                         {touched.lastName && errors.lastName && (<p className='form__error'>{errors.lastName}</p>)}
-                        <Field
-                            className='field'
-                            type='text'
-                            name='email'
-                            placeholder='Email'
-                        />
-                         {touched.email && errors.email && (<p className='form__error'>{errors.email}</p>)} 
-                        <Field
-                            className='field'
-                            type='password'
-                            name='password'
-                            placeholder='Password (required)'
-                        />
-                         {touched.password && errors.password && (<p className='form__error'>{errors.password}</p>)}
-                        <Field
-                            className='field'
-                            type='text'
-                            name='phone'
-                            placeholder='Phone (required)'
-                        />
-                         {touched.phone && errors.phone && (<p className='form__error'>{errors.phone}</p>)}
-                        <Field
-                            className='field'
-                            type='text'
-                            name='street'
-                            placeholder='Street Name'
-                        />
-                         {touched.street && errors.street && (<p className='form__error'>{errors.street}</p>)}
-                        <Field
-                            className='field'
-                            type='text'
-                            name='city'
-                            placeholder='City Name (Required)'
-                        />
-                        {touched.city && errors.city && (<p className='form__error'>{errors.city}</p>)}
-                        <Field 
-                            className='field'
-                            type='text'
-                            name='state'
-                            placeholder='State'
-                        />
-                         {touched.state && errors.state && (<p className='form__error'>{errors.state}</p>)}
-                        <Field 
-                            className='field'
-                            type='text'
-                            name='avatarUrl'
-                            placeholder='Avatar'
-                        />
-                         {touched.avatarUrl && errors.avatarUrl && (<p className='form__error'>{errors.avatarUrl}</p>)} 
-                        <Button className="button" type="submit" disabled={isSubmitting}>Submit</Button>
+                        <Segment stacked>
+                            <Field
+                                className='field'
+                                type='text'
+                                name='firstName'
+                                placeholder='First Name'
+                            />
+                             {touched.firstName && errors.firstName && (<p className='form__error'>{errors.firstName}</p>)}
+                            <Field
+                                className='field'
+                                type='text'
+                                name='lastName'
+                                placeholder='Last Name'
+                            />
+                             {touched.lastName && errors.lastName && (<p className='form__error'>{errors.lastName}</p>)}
+                            <Field
+                                className='field'
+                                type='text'
+                                name='email'
+                                placeholder='Email'
+                            />
+                             {touched.email && errors.email && (<p className='form__error'>{errors.email}</p>)} 
+                            <Field
+                                className='field'
+                                type='password'
+                                name='password'
+                                placeholder='Password (required)'
+                            />
+                             {touched.password && errors.password && (<p className='form__error'>{errors.password}</p>)}
+                            <Field
+                                className='field'
+                                type='text'
+                                name='phone'
+                                placeholder='Phone (required)'
+                            />
+                             {touched.phone && errors.phone && (<p className='form__error'>{errors.phone}</p>)}
+                            <Field
+                                className='field'
+                                type='text'
+                                name='street'
+                                placeholder='Street Name'
+                            />
+                             {touched.street && errors.street && (<p className='form__error'>{errors.street}</p>)}
+                            <Field
+                                className='field'
+                                type='text'
+                                name='city'
+                                placeholder='City Name (Required)'
+                            />
+                            {touched.city && errors.city && (<p className='form__error'>{errors.city}</p>)}
+                            <Field 
+                                className='field'
+                                type='text'
+                                name='state'
+                                placeholder='State'
+                            />
+                             {touched.state && errors.state && (<p className='form__error'>{errors.state}</p>)}
+                            <Field 
+                                className='field'
+                                type='text'
+                                name='avatarUrl'
+                                placeholder='Avatar'
+                            />
+                             {touched.avatarUrl && errors.avatarUrl && (<p className='form__error'>{errors.avatarUrl}</p>)} 
+                             <Button 
+                                 style={{
+                                   color: 'white',
+                                   backgroundColor: '#2B4162',
+                                   marginTop: '10px'
+                                 }}
+                                 fluid
+                                 size="large"
+                                 type="submit" 
+                                 disabled={isSubmitting}
+                             >
+                                 Submit
+                            </Button>
+                        </Segment>
                     </Form>
                 )}        
             </Formik>         
-        </Container>
+        </Grid.Column>
+    </Grid>
     );
 }
 
-                export default CreateProfile;
+export default CreateProfile;
