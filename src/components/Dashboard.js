c288c0d26fabca3f53179a41d629249a49c44f0a
seongck <seongk@pm.me>
2019-08-27 17:29:44 -0700

    Add default avatar and welcome message

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Header, Image } from 'semantic-ui-react';
 import Navigation from './Navigation.js';
+import defaultAvatar from '../assets/default-avatar.png';
 
 function Dashboard(props) {
+  console.log(props);
+  const avatarSrc = props.user.avatarUrl ? props.user.avatarUrl : defaultAvatar;
+
   return (
     <Container fluid>
       <Navigation />
-      <h1>Dashboard</h1>
+        <Container text textAlign='center'>
+          <Image src={avatarSrc} verticalAlign='middle' size='tiny' circular/>
+          <Header as="h2">
+            Welcome back {props.user.firstName}!
+          </Header>
+        </Container>
     </Container>
     
   );
