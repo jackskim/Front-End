1dbae10589fc0ead139b0286b844db1edad34100
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-08-29 18:49:26 -0500

    added cardlist to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import Navigation from './Navigation.js';
+import ItemCardList from './ItemCardList';
 
 function Home(props) {
   return (
     <Container fluid>
       <Navigation />
+      < ItemCardList />
     </Container>
   );
 }
