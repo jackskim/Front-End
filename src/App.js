70c15dbf964424782699f300797c8d75921533d6
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-08-29 14:24:58 -0500

    followed jonathans help and believe the cards have it to where when you click on them it renders the cardInfo component might need minor fixes tho

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Login from './components/Login.js';
 import SignUp from './components/SignUp.js';
 import Home from './components/Home.js';
+import CreateProfile from './components/CreateProfile';
 import PrivateRoute from './components/PrivateRoute.js';
 import Logout from './components/Logout.js';
-import ItemCardList from './components/ItemCardList.js';
 
 function App() {
+  const [user, setUser] = useState({});
+
   return (
     <div className="App">
-      < ItemCardList />
+      <PrivateRoute
+        exact
+        path="/"
+        component={Home}
+      />
+      <Route
+        exact
+        path="/login"
+        render={props => <Login {...props} setUser={setUser} />}
+      />
+      <Route
+        exact
+        path="/signup"
+        render={props => <SignUp {...props} setUser={setUser} />}
+      />
+      <Route
+        exact
+        path="/logout"
+        component={Logout}
+      />
+      <Route
+        exact
+        path="/createprofile"
+        render={props => <CreateProfile {...props} user={user} />}
+      />
+      <Route
+        path="/cards/:id" 
+        component={Card}
+      />
+      <Route
+        path="/itemcardinfo"
+        render={props => <ItemCardInfo {...props} setItem={setItem} />}
+      />
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Card = props => {
+
+    const [item, setItem] = useState();
+
+    useEffect(() => {
+      const id = props.match.params.id;
+      (async () => {
+        try {
+          const response = await fetch(`https://umts-backend.herokuapp.com/api/rentItems/${id}`);
+          const data = await response.json();
+          setItem(data);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }, []);
+  
+    if (!item) return <p>Loading...</p>;
+  
     return (
-        <div className='card-container'>
-            <img src={props.imageUrl} />
-            <h1>{props.name}</h1>
-            <h3>{props.address}</h3>
-            <h3>{props.price}</h3>
+        <div className='card-container' onClick={props.history.push(`/rentitems/${id}`)}>
+        <img src={props.imageUrl} />
+        <h1>{props.name}</h1>
+        <h3>{props.address}</h3>
+        <h3>{props.price}</h3>
         </div>
     )
 };
