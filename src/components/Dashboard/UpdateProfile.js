0063534e8c3c65d3b84c3f47f9b59c7f419e79de
seongck <seongk@pm.me>
2019-08-29 15:48:59 -0700

    Remove unused imports

--- src/components/Dashboard/UpdateProfile.js
+++ src/components/Dashboard/UpdateProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Formik, Form, Field, } from 'formik';
 import * as Yup from 'yup';
 import { Grid, Segment, Header, Button } from 'semantic-ui-react';
