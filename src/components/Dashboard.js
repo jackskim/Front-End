26bf5721d53a2001b87d7758f0dd8722ea4317fa
seongck <seongk@pm.me>
2019-08-28 21:23:53 -0700

    Add user item list display

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Container, Header, Image, Menu, Tab } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+//import axiosWithAuth from '../axiosWithAuth.js';
+import axios from 'axios';
+import { Container, Header, Image, Menu, Tab, List } from 'semantic-ui-react';
+
 import Navigation from './Navigation.js';
 import defaultAvatar from '../assets/default-avatar.png';
 
 function Dashboard(props) {
+
   const avatarSrc = props.user.avatarUrl ? props.user.avatarUrl : defaultAvatar;
+
+  const [userItems, setUserItems] = useState([]);
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const response = await axios.get('https://umts-backend.herokuapp.com/api/rentitems');
+        const allItems = response.data.rentItems;
+        setUserItems(allItems.filter( item => item.userId === 8));
+        // REMEMBER TO CHANGE BACK 8 to props.user.id !!!
+      } catch(error) {
+        console.log(error);
+      }
+    })(); 
+  },[]);
+
+  console.log(userItems);
 
   const panes = [
     { menuItem: ( 
@@ -12,15 +33,37 @@ function Dashboard(props) {
         My Item List
       </Menu.Item>
     ),
-    render: () => <Tab.Pane></Tab.Pane>
+      render: () => {
+        return (
+          <Tab.Pane>
+            <List style={{textAlign:"left"}}>
+              {userItems.map( item => {
+                return (
+                  <List.Item>
+                    {item.name}
+                  </List.Item>
+                );
+              })}
+            </List>
+          </Tab.Pane>
+        );
+      }
+    },
+    { menuItem: ( 
+      <Menu.Item key='update-item'>
+        Update Item 
+      </Menu.Item>
+    ),
+      render: () => <Tab.Pane></Tab.Pane>
     },
     { menuItem: ( 
       <Menu.Item key='update-profile'>
         Update Profile 
       </Menu.Item>
     ),
-    render: () => <Tab.Pane></Tab.Pane>
+      render: () => <Tab.Pane></Tab.Pane>
     }
+
   ];
 
   return (
