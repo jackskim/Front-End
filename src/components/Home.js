624d72f7c656596be35871e46389707f8e1f51c0
macjabeth <macbethjonathan@gmail.com>
2019-09-03 16:29:45 -0400

    add shared search state to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import Navigation from './Navigation.js';
 import ItemCardList from './ItemCardList';
 
 function Home(props) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchInput = (e) => setSearchInput(e.target.value);
+
   return (
     <Container fluid>
-      <Navigation user={props.user} setUser={props.setUser}/>
-      <ItemCardList />
+      <Navigation searchInput={searchInput} handleSearchInput={handleSearchInput} />
+      <ItemCardList searchInput={searchInput} />
 
     </Container>
   );
