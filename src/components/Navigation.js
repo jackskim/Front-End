72132d6cb4d533f0ebe80eea66159a534a8c65b6
macjabeth <macbethjonathan@gmail.com>
2019-09-03 16:27:14 -0400

    add search filter to navigation & make it fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import { Container, Input, Menu } from 'semantic-ui-react';
 
-function Navigation(props) {
+function Navigation({ searchInput, handleSearchInput }) {
   return (
-    <Menu>
-      <Menu.Item key='logo'>
-        <NavLink to="/">TSTUFF</NavLink>
-      </Menu.Item>
-      <Menu.Item key='home'>
-        <NavLink to="/">Home</NavLink>
-      </Menu.Item>
-      <Menu.Item key='dashboard'>
-        <NavLink to={`/dashboard`}>Dashboard</NavLink>
-      </Menu.Item>
-      <Menu.Item key='list-item'>
-        <NavLink to="/rentoutitem">List Item</NavLink>
-      </Menu.Item> 
-      <Menu.Item key='logout'>
-        <NavLink to="/logout">Log Out</NavLink>
-      </Menu.Item> 
+    <Menu pointing className='fixed'>
+      <Container>
+        {/*<Menu.Item key='logo'>*/}
+        {/*  <NavLink to="/">TSTUFF</NavLink>*/}
+        {/*</Menu.Item>*/}
+        <Menu.Item key='home'>
+          <NavLink exact to="/">Home</NavLink>
+        </Menu.Item>
+        <Menu.Item key='dashboard'>
+          <NavLink to={`/dashboard`}>Dashboard</NavLink>
+        </Menu.Item>
+        <Menu.Item key='list-item'>
+          <NavLink to="/rentoutitem">List Item</NavLink>
+        </Menu.Item>
+        <Menu.Item key='logout'>
+          <NavLink to="/logout">Log Out</NavLink>
+        </Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' onChange={handleSearchInput} value={searchInput} />
+          </Menu.Item>
+        </Menu.Menu>
+      </Container>
     </Menu>
   );
 }
