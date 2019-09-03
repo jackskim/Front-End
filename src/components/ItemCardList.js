04f8770c2e9a897abb17360ba4edd41ed6be4d7f
macjabeth <macbethjonathan@gmail.com>
2019-09-03 16:29:23 -0400

    filter cards by navigation search input & tweak header margin

--- src/components/ItemCardList.js
+++ src/components/ItemCardList.js
@@ -1,38 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Container, Header } from 'semantic-ui-react';
 import ItemCardInfo from './ItemCardInfo';
 import '../index.css';
 
-function ItemCardList(props) {
-    const [itemCards, setItemCards] = useState([]);
+function ItemCardList({ searchInput }) {
+  let [itemCards, setItemCards] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get("https://umts-backend.herokuapp.com/api/rentItems/")
-            .then(res => {
-                setItemCards(res.data.rentItems);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('https://umts-backend.herokuapp.com/api/rentItems/')
+      .then(res => {
+        setItemCards(res.data.rentItems);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <Container fluid >
-          <Header as="h1" textAlign="center" style={{margin: '50px auto 20px'}}> Items for Rent</Header>
-            <div className='itemcard-list grid-view'>
-                {itemCards.map(itemCard => {
-                    return (
-                        <Link key={itemCard.id} to={`/card/${itemCard.id}`}>
-                            <ItemCardInfo {...itemCard} />
-                        </Link>
-                    )
-                })}
-            </div>
-        </Container>
-    );
+  if (searchInput) itemCards = itemCards.filter(card => card.name.includes(searchInput) || card.description.includes(searchInput));
+
+  return (
+    <Container fluid>
+      <Header as="h1" textAlign="center" style={{ margin: '75px auto 20px' }}> Items for Rent</Header>
+      <div className='itemcard-list grid-view'>
+        {itemCards.map(itemCard => {
+          return (
+            <Link key={itemCard.id} to={`/card/${itemCard.id}`}>
+              <ItemCardInfo {...itemCard} />
+            </Link>
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
 
 export default ItemCardList;
